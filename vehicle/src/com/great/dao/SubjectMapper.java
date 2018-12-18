e919210239c192d68fb55634bee8ea6ff33b1837
410494213@qq.com <33885423+hhjjxxgithub@users.noreply.github.com>
2018-12-18 19:53:05 +0800

    Revert "1"
    This reverts commit c970f8c1042b1e3ad8d47472f9da264f74248d25.

---
+++ vehicle/src/com/great/dao/SubjectMapper.java
@@ -0,0 +1,18 @@
+package com.great.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.great.bean.Coach;
+import com.great.bean.Subject;
+
+
+public interface SubjectMapper {
+	public int upload(Map map) throws Exception;
+	public int add (Subject subject) throws Exception;
+	public int update (Subject subject) throws Exception;
+	public int delete (Integer subjectId) throws Exception;
+	public int reset (Integer subjectId) throws Exception;
+	public List queryAll() throws Exception;
+	public Subject getById(Integer subjectId) throws Exception;
+}
